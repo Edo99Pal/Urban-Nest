17a8d7352a24af370b6f2e95444be1856e7d8d87
edo99pal <edo99pal@gmail.com>
2023-12-10 21:11:32 +0100

    Updates

---
+++ app/services/page-changer.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BookingService } from './booking.service';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PageChangerService {
+  page: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  innerWidth: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  constructor(private _bookingService: BookingService) {
+  }
+
+  onPageChange(n: number) {
+    this.page.next(n);
+    this.innerWidth.next(window.innerWidth);
+    this.page.value == 2 ? this._bookingService.showed = true : null;
+    window.scrollTo({
+      top: 0
+    });
+  }
+
+}
